4acb42c2b69f1b59c9b13e9146c7c4078389889d
Ted64sar <fedor-commander@yandex.ru>
2019-04-04 23:38:04 +0300

    Лиза, смотри в файл PARLALALA, это то, что присылала Лапшева, и оно более или менее запускается

--- books_form.py
+++ books_form.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class BooksForm(FlaskForm):
-    title = StringField('Название и автор', validators=[DataRequired()])
-    content = TextAreaField("Аннотация", validators=[DataRequired()])
+    title = StringField('Ссылка', validators=[DataRequired()])
+    content = TextAreaField("Краткое описание", validators=[DataRequired()])
     year = StringField('Год написания', validators=[DataRequired()])
     submit = SubmitField('Добавить')
